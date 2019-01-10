d30f38e941215bc6b454679a2b4562537eead5f8
John <splaysing@gmail.com>
2019-01-10 22:42:02 +0200

    Массивы/функции/шаблонизатор

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+//Функцию-шаблонизатор
+function renderTemplate($templatePath, $templateData = []) {
+    $content = '';
+    extract($templateData);
+    if (file_exists($templatePath)) {
+        ob_start();
+        require($templatePath);
+        $content = ob_get_contents();
+        ob_end_clean();
+    }
+    return $content;
+}
+?>
